f8f77bf23c77a51d4e61a777848a5d0bb4d6344e
Luke Heinrichs <lukeheinrichs21@augustana.edu>
2024-01-16 00:47:30 -0600

    Updated TileSet Collisions and made enemy switch to run animation when moving

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -15,6 +15,7 @@ func _physics_process(delta: float):
 		animated_sprite.flip_h = false
 	velocity = direction * speed
 	move_and_slide()
+	update_animations(direction)
 	
 func make_path():
 	navigation_agent.target_position = player.global_position + Vector2(5,5)
@@ -22,3 +23,9 @@ func make_path():
 
 func _on_timer_timeout():
 	make_path()
+	
+func update_animations(direction):
+	if direction == Vector2.ZERO:
+		animated_sprite.play("idle")
+	else:
+		animated_sprite.play("run")
