54d66754aa5a5de07cf184a023a0014720122a48
Luke Heinrichs <lukeheinrichs21@augustana.edu>
2024-01-16 00:36:14 -0600

    Added a generic enemy with loose navigation

---
+++ Scripts/enemy.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+const speed = 70
+
+@export var player: Node2D
+
+@onready var navigation_agent := $NavigationAgent2D
+@onready var animated_sprite = $AnimatedSprite2D
+
+func _physics_process(delta: float):
+	var direction = to_local(navigation_agent.get_next_path_position()).normalized()
+	if direction.x < 0:
+		animated_sprite.flip_h = true
+	if direction.x > 0:
+		animated_sprite.flip_h = false
+	velocity = direction * speed
+	move_and_slide()
+	
+func make_path():
+	navigation_agent.target_position = player.global_position + Vector2(5,5)
+
+
+func _on_timer_timeout():
+	make_path()
