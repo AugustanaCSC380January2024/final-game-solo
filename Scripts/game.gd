f7bdb867029f407c9d0c11eacad5e9cdef611ea9
Luke Heinrichs <lukenheinrichs@gmail.com>
2024-01-18 14:36:53 -0600

    Added enemy spawners

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -1,1 +1,28 @@
 extends Node2D
+
+@onready var level = $Level/SpawnAreas
+@onready var player = get_node("Player")
+@onready var beacon = get_node("Beacon")
+
+var spawn_areas = []
+
+func _ready():
+	get_spawn_areas()
+	
+func get_spawn_areas():
+	for spawn_area in level.get_children():
+		spawn_areas.append(spawn_area)
+
+func clear_spawn_area():
+	spawn_areas.clear()
+	
+func spawn_enemies(amount, player, beacon):
+	for enemy_num in amount:
+		var random_spawn = spawn_areas.pick_random()
+		random_spawn.spawn_enemy(player, beacon)
+
+
+
+
+func _on_timer_timeout():
+	spawn_enemies(4, player, beacon)
--- Scripts/spawn_area.gd
+++ Scripts/spawn_area.gd
@@ -2,5 +2,19 @@ extends Area2D
 
 @onready var collision_shape_2d = $CollisionShape2D
 
-var centerpos = collision_shape_2d.position + self.position
+var small_enemy = preload("res://Scenes/enemy.tscn")
+var large_enemy = preload("res://Scenes/large_enemy.tscn")
+
+var enemy_array = []
+
+func _ready():
+	enemy_array.append(small_enemy)
+	enemy_array.append(large_enemy)
+
+func spawn_enemy(player, beacon):
+	print("enemy spawned")
+	var spawnable_enemy = enemy_array.pick_random().instantiate()
+	spawnable_enemy.player = player
+	spawnable_enemy.beacon = beacon
+	add_child(spawnable_enemy)
 
